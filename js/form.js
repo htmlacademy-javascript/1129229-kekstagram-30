b0184f687b3aa3db573a0c8b5341a72ed4862886
DespaiiR <vlad@hozin.ru>
2023-11-26 14:38:34 +0300

    Помощь друга

--- js/form.js
+++ js/form.js
@@ -1,20 +1,43 @@
 import {resetData, isValidation} from './form-valid.js';
+import {resetFilters, renderFilters} from './form-silder.js';
+import {DEFOULT_SCOPE} from './subject.js';
+import {resetToDefault} from './scope.js';
 const form = document.querySelector('.img-upload__form');
 const formInput = form.querySelector('.img-upload__input');
 const formOverlay = form.querySelector('.img-upload__overlay');
 const formCancel = form.querySelector('.img-upload__cancel');
+const formPreview = form.querySelector('.img-upload__preview img');
+const previewEffects = document.querySelectorAll('.effects__preview');
 
 const showEditModal = () => {
   if (formInput.files.length !== 0) {
     formOverlay.classList.remove('hidden');
     document.body.classList.add('modal-open');
-
     document.addEventListener('keydown', onEditModalEscKeydown);
   }
 };
 
+const renderModalPhoto = () => {
+  const fileImage = formInput.files[0];
+  formPreview.src = URL.createObjectURL(fileImage);
+  formPreview.style.transform = `scale(${DEFOULT_SCOPE.VALUE})`;
+};
+
+const renderMiniPreviews = () => {
+  const fileImage = formInput.files[0];
+  const url = URL.createObjectURL(fileImage);
+  previewEffects.forEach((previewMini) => {
+    previewMini.style.backgroundImage = `url(${url})`;
+  });
+};
+
 const onFileInputChange = () => {
   showEditModal();
+  renderModalPhoto();
+  renderMiniPreviews();
+  resetToDefault();
+  resetFilters();
+  renderFilters();
 };
 
 const hideEditModal = () => {
@@ -51,7 +74,7 @@ function removeEscListener() {
 formInput.addEventListener('change', () => onFileInputChange());
 form.addEventListener('submit', (evt) => {
   if (isValidation()) {
-    //форма валидна
+    //валидно
   } else {
     evt.preventDefault();
   }
--- js/subject.js
+++ js/subject.js
@@ -1,13 +1,3 @@
-const COMMENTS_TO_SHOW = 5;
-const MAX_PICTURE_POSTS = 25;
-const COMMENT_LENGTH = 140;
-const MAX_HASHTAGS = 5;
-const HASHTAGS_ERRORS_TEXT = {
-  INVALID_COUNT: 'Не должно быть больше 5 хэштэгов',
-  INVALID_CORRECT: 'некорректный хэштэг',
-  NOT_UNIQUE: 'Ошибочный хэштэг',
-};
-
 const USERS_NAMES = [
   'Rodney',
   'Larry',
@@ -39,4 +29,92 @@ const PHOTOS_DESCRIPTION = [
   'No matter what I do, I like everything'
 ];
 
-export {COMMENTS_TO_SHOW, MAX_PICTURE_POSTS, USERS_NAMES, USERS_COMMENTS, PHOTOS_DESCRIPTION, COMMENT_LENGTH, MAX_HASHTAGS, HASHTAGS_ERRORS_TEXT};
+const COMMENTS_TO_SHOW = 5;
+const MAX_PICTURE_POSTS = 25;
+const COMMENT_LENGTH = 140;
+const COMMENT_LENGTH_ERROR = 'Не более 140 символов';
+const MAX_HASHTAGS = 5;
+const HASHTAGS_ERRORS_TEXT = {
+  INVALID_COUNT: 'Не должно быть больше 5 хэштэгов',
+  INVALID_CORRECT: 'некорректный хэштэг',
+  NOT_UNIQUE: 'Ошибочный хэштэг',
+};
+
+const SCOPE_STEP = {
+  PERCENTS: 25,
+  VALUE: 0.25,
+};
+const MAX_SCOPE = {
+  PERCENTS: 100,
+  VALUE: 1,
+};
+const MIN_SCOPE = {
+  PERCENTS: 25,
+  VALUE: 0.25,
+};
+const DEFOULT_SCOPE = {
+  PERCENTS: 100,
+  VALUE: 1,
+};
+
+const FilterEffects = {
+  none: 'None',
+  chrome: 'Chrome',
+  sepia: 'Sepia',
+  marvin: 'Marvin',
+  phobos: 'Phobos',
+  heat: 'Heat'
+};
+
+const Filters = {
+  None: {
+    effect: 'none',
+    effectValue: 0,
+    min: 0,
+    max: 0,
+    step: 0,
+    unit: '',
+  },
+  Chrome: {
+    effect: 'grayscale',
+    effectValue: 1,
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  Sepia: {
+    effect: 'sepia',
+    effectValue: 1,
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  Marvin: {
+    effect: 'invert',
+    effectValue: 1,
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  Phobos: {
+    effect: 'blur',
+    effectValue: 1,
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  Heat: {
+    effect: 'brightness',
+    effectValue: 1,
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  },
+};
+
+export {COMMENTS_TO_SHOW, MAX_PICTURE_POSTS, USERS_NAMES, USERS_COMMENTS, PHOTOS_DESCRIPTION, COMMENT_LENGTH, COMMENT_LENGTH_ERROR, MAX_HASHTAGS, HASHTAGS_ERRORS_TEXT, SCOPE_STEP, MAX_SCOPE, MIN_SCOPE, DEFOULT_SCOPE, FilterEffects, Filters};
